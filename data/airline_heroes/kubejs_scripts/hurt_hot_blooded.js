963c182e753d63af824f152e0683bcd15ae8e68a
aflowers1101-lab <aflowers1101@gmail.com>
2025-11-11 18:37:27 -0500

    HELPP

--- data/airline_heroes/kubejs_scripts/hurt_hot_blooded.js
+++ data/airline_heroes/kubejs_scripts/hurt_hot_blooded.js
@@ -1,8 +1,8 @@
 EntityEvents.hurt((event) => {
-    if (!event.source.damage) return
+    if (!event.source.actual) return
 
-     if (event.source.damage.player) {
-        let player = event.source.damage
+     if (event.source.actual.player) {
+        let player = event.source.actual
         let hurtEntity = event.entity
 		    const scoreboard = player.server.getScoreboard();
             const objective = scoreboard.getObjective('airline_heroes.heat_build_up');
@@ -12,6 +12,12 @@ EntityEvents.hurt((event) => {
 			    let pos = hurtEntity.blockPosition().mutable()
 				let posfixed = pos.y+1
         player.server.runCommandSilent(`particle airline_heroes:air_punch ${pos.x} ${posfixed} ${pos.z} 0 0 0 0 5 force`);
+        };
+		if (palladium.abilities.isEnabled(hurtEntity, "airline_heroes:hot_blooded", "hot_blooded_build_up")) {
+            scoreboard.getOrCreatePlayerScore(hurtEntity.username, objective).add(500);
+			    let pos = player.blockPosition().mutable()
+				let posfixed = pos.y+1
+        hurtEntity.server.runCommandSilent(`particle airline_heroes:air_punch ${pos.x} ${posfixed} ${pos.z} 0 0 0 0 5 force`);
         }
     }
 });
