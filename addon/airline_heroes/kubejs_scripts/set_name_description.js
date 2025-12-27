e9fcc59972a5892136e689b45c35d3ad54314dc7
Splitz <53245708+Splitz-1@users.noreply.github.com>
2025-12-27 09:27:21 -0800

    AFO finally T_T
    Added
      Able to transfer single powers as afo
      Able to trasnfer all powers as afo

--- addon/airline_heroes/kubejs_scripts/set_name_description.js
+++ addon/airline_heroes/kubejs_scripts/set_name_description.js
@@ -1,17 +1,28 @@
-StartupEvents.registry('palladium:abilities', event => {
-    event.create('airline_heroes:set_name_description')
-        .icon(palladium.createItemIcon('minecraft:beacon'))
-        .displayName('Set Name And Description')
-        .documentationDescription('Potion area of effect')
-        .addProperty('power_name', 'string', 'Example Name', 'Superpower name used in skill tree')
-		.addProperty('power_description', 'string', 'Example Description', 'Superpower description used in skill tree')
-        .firstTick((entity, entry, holder, enabled) => {
-            if (!enabled) return;
-            if (!entity.level || entity.level.isClientSide()) return;
+StartupEvents.registry("palladium:abilities", (event) => {
+  event
+    .create("airline_heroes:set_name_description")
+    .icon(palladium.createItemIcon("minecraft:beacon"))
+    .displayName("Set Name And Description")
+    .documentationDescription("Potion area of effect")
+    .addProperty(
+      "power_name",
+      "string",
+      "Example Name",
+      "Superpower name used in skill tree"
+    )
+    .addProperty(
+      "power_description",
+      "string",
+      "Example Description",
+      "Superpower description used in skill tree"
+    )
+    .firstTick((entity, entry, holder, enabled) => {
+      if (!enabled) return;
+      if (!entity.level || entity.level.isClientSide()) return;
 
-        const name = entry.getPropertyByName('power_name');
-		const description = entry.getPropertyByName('power_description');
-        palladium.setProperty(entity, 'name', name);
-        palladium.setProperty(entity, 'description', description);
-        });
+      const name = entry.getPropertyByName("power_name");
+      const description = entry.getPropertyByName("power_description");
+      palladium.setProperty(entity, "name", name);
+      palladium.setProperty(entity, "description", description);
+    });
 });
---
+++ data/airline_heroes/kubejs_scripts/properties.js
@@ -0,0 +1,19 @@
+PalladiumEvents.registerProperties((event) => {
+  if (event.getEntityType() === "minecraft:player") {
+    event.registerProperty("name", "string", "Example Name");
+    event.registerProperty("description", "string", "Example Description");
+    event.registerProperty("powerCount", "integer", 0);
+    event.registerProperty("powerSelect", "integer", 0);
+    event.registerProperty("collectionSelector", "integer", 1);
+    event.registerProperty("pSlot1", "string", "empty");
+    event.registerProperty("pSlot2", "string", "empty");
+    event.registerProperty("pSlot3", "string", "empty");
+    event.registerProperty("pSlot4", "string", "empty");
+    event.registerProperty("pSlot5", "string", "empty");
+    event.registerProperty("pSlot6", "string", "empty");
+    event.registerProperty("pSlot7", "string", "empty");
+    event.registerProperty("pSlot8", "string", "empty");
+    event.registerProperty("pSlot9", "string", "empty");
+    event.registerProperty("pSlot10", "string", "empty");
+  }
+});
