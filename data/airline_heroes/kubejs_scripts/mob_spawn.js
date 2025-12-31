f5bafff98aabb82cbb8938439c7a1dca9b5c1517
Splitz <53245708+Splitz-1@users.noreply.github.com>
2025-12-31 06:25:52 -0800

    Mob Powers
    Added
      Infinity, just pushes nearby entities
      Ocular beams player power texture
      buyable stats for nomu

--- data/airline_heroes/kubejs_scripts/mob_spawn.js
+++ data/airline_heroes/kubejs_scripts/mob_spawn.js
@@ -3,9 +3,10 @@ EntityEvents.spawned((event) => {
     "minecraft:zombie",
     "minecraft:skeleton",
     "minecraft:creeper",
-    "minecraft:spider",
+    "minecraft:witch",
     "minecraft:husk",
     "minecraft:stray",
+    "minecraft:wither_skeleton",
   ];
   let powerlist = [
     "airline_heroes:mob/2x_stregth",
@@ -14,8 +15,9 @@ EntityEvents.spawned((event) => {
     "airline_heroes:mob/frigiokinesis",
     "airline_heroes:mob/headcannon",
     "airline_heroes:mob/immortality",
+    "airline_heroes:mob/infinity",
     "airline_heroes:mob/intangibility",
-    "airline_heroes:mob/ocular_mimicry",
+    "airline_heroes:mob/ocular_beams",
     "airline_heroes:mob/shadow_fade",
     "airline_heroes:mob/speedster",
     "airline_heroes:mob/tactile_pyrokinesis",
