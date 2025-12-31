504ecf66e718fe77856ba7831095dda3f8c85e14
Splitz <53245708+Splitz-1@users.noreply.github.com>
2025-12-31 12:19:10 -0800

    OFA
    Ofa now lets you keep your ability, however you have to specific kept ability in the ofa power file under the blacklist section, so before passing on the ability, to a new person, the black listed power must be updated to the users original power.

--- addon/airline_heroes/kubejs_scripts/ofa.js
+++ addon/airline_heroes/kubejs_scripts/ofa.js
@@ -10,6 +10,12 @@ StartupEvents.registry("palladium:abilities", (event) => {
       "powers that can be passed on"
     )
     .addProperty(
+      "blacklist",
+      "string",
+      "aireline_heroes:ergo",
+      "The power to keep with the passing of OFA"
+    )
+    .addProperty(
       "player_only",
       "boolean",
       true,
@@ -21,25 +27,32 @@ StartupEvents.registry("palladium:abilities", (event) => {
       "airline_heroes.pastuser",
       "the tag that is applied to the entity when this ability is active."
     )
-    .tick((entity, entry, enabled) => {
+    .firstTick((entity, entry, enabled) => {
       if (enabled && entity.isPlayer()) {
         let range = entry.getPropertyByName("range");
         let whitelist = entry.getPropertyByName("whitelist");
         let tag = entry.getPropertyByName("tag");
         let target = entity.rayTrace(range).entity;
         let playerOnly = entry.getPropertyByName("player_only");
+        let blacklist = entry.getPropertyByName("blacklist");
         if (playerOnly === true && (target === null || !target.isPlayer()))
           return;
-        if (target !== null && !target.tags.contains(tag)) {
+        if (
+          target !== null &&
+          !target.tags.contains(tag) &&
+          !target.tags.contains("airline_heroes.afo")
+        ) {
           whitelist.forEach((p) => {
             if (
               palladium.superpowers.hasSuperpower(entity, p) &&
-              !palladium.superpowers.hasSuperpower(target, p)
+              !palladium.superpowers.hasSuperpower(target, p) &&
+              p != blacklist
             ) {
               palladium.superpowers.addSuperpower(target, p);
               palladium.superpowers.removeSuperpower(entity, p);
             } else return;
           });
+          palladium.superpowers.addSuperpower(target, blacklist);
           entity.tags.add(tag);
         }
       }
