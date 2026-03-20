8a996b992954346d42221311e10dc26531d9af37
aflowers1101-lab <aflowers1101@gmail.com>
2026-03-20 15:25:51 -0400

    sounds added to some of muses abilities and mask now has an item texture

--- addon/airline_heroes/kubejs_scripts/sounds.js
+++ addon/airline_heroes/kubejs_scripts/sounds.js
@@ -1,3 +1,9 @@
 StartupEvents.registry('sound_event', event => {
     event.create('airline_heroes:half_mass_form');
+    event.create('airline_heroes:comedy_push');
+    event.create('airline_heroes:tragedy_invis');
+    event.create('airline_heroes:comedy_aoe');
+    event.create('airline_heroes:tragedy_aoe');
+
+
 })
