dac802d780ea61d2cfab838e7aec3a2e04d946b0
craftmayham <craftmayham@gmail.com>
2026-05-21 02:29:13 -0400

    Re-added non-relative move player

--- addon/airline_heroes/kubejs_scripts/launch_player.js
+++ addon/airline_heroes/kubejs_scripts/launch_player.js
@@ -6,17 +6,42 @@ StartupEvents.registry('palladium:abilities', event => {
         .displayName('Launch Forward')
         .documentationDescription('Launches the player in the direction they are looking.')
         .addProperty('power', 'double', 2.0, 'Launch strength multiplier')
- .tick((entity, entry, holder, enabled) => {
+        .tick((entity, entry, holder, enabled) => {
             if (!enabled) return;
- 
-            const lookVec = entity.getLookAngle(); 
+
+            const lookVec = entity.getLookAngle();
             const launchPower = entry.getPropertyByName('power');
             const motion = lookVec.scale(launchPower);
-                   
-					 entity.setDeltaMovement(motion);
+
+            entity.setDeltaMovement(motion);
             // Apply velocity in the look direction      
-			              if (entity.isPlayer()) {
-                        entity.connection.send(new ClientboundSetEntityMotionPacket(entity));
-                    }
+            if (entity.isPlayer()) {
+                entity.connection.send(new ClientboundSetEntityMotionPacket(entity));
+            }
+        });
+});
+
+StartupEvents.registry("palladium:abilities", (event) => {
+    event
+        .create("airline_heroes:move_player")
+        .icon(palladium.createItemIcon("minecraft:elytra"))
+        .displayName("Move Player")
+        .documentationDescription("Moves a player in a non-relative direction.")
+        .addProperty("x", "double", 0.0, "X strength multiplier")
+        .addProperty("y", "double", 2.0, "Y strength multiplier")
+        .addProperty("z", "double", 0.0, "Z strength multiplier")
+        .tick((entity, entry, holder, enabled) => {
+            if (!enabled) return;
+
+            const x = entry.getPropertyByName("x");
+            const y = entry.getPropertyByName("y");
+            const z = entry.getPropertyByName("z");
+
+            let newVec3 = new $Vec3(x, y, z);
+            entity.setDeltaMovement(newVec3);
+            // Apply velocity
+            if (entity.isPlayer()) {
+                entity.connection.send(new ClientboundSetEntityMotionPacket(entity));
+            }
         });
 });
